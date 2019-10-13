8aa41e664954003f1b27e0c3762081080a0f4359
amitdhanke <amit.dhanke8@gmail.com>
2019-10-13 20:48:13 +0530

    committing beautiful protractor report script

--- Config.js
+++ Config.js
@@ -1,5 +1,22 @@
+//for reporting lib
+var HtmlReporter = require('protractor-beautiful-reporter');
+
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
+   directConnect: true,
+
+   capabilities: {
+	    browserName: 'chrome'
+	  },
+   framework: 'jasmine',
+
   //specs: ['src/scripts/FirstProtractorScript.js','src/scripts/SecoundProtractorScript.js']
-  specs: ['src/scripts/SecoundProtractorScript.js']
+  specs: ['src/scripts/ThirdProtractorScript.js'],
+  
+  onPrepare: function() {
+      // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+      jasmine.getEnv().addReporter(new HtmlReporter({
+         baseDirectory: 'Reports/screenshots'
+      }).getJasmine2Reporter());
+   }
 };
---
+++ src/scripts/ThirdProtractorScript.js
@@ -0,0 +1,48 @@
+describe("Drop Down Testsuite",function(){
+	
+	it("SPEC01",function(){
+		
+		browser.get("https://angularjs.org/");
+		
+		browser.manage().window().maximize();
+		
+		element(by.linkText("Find out more.")).click();
+		
+		//get the all options from the drop down..
+		
+		element.all(by.options("v as v.label group by v.group for v in $ctrl.versions")).then(function(index){
+			
+			for(var i = 0; i < index.length ; i++)
+			{
+				if(i == 3)
+				{
+					//click on the specific index element
+					index[i].click();
+				}
+			}
+		});
+		
+		
+		
+		expect(element(by.xpath("//option[@selected='selected']")).getText()).toEqual("v1.6.10");
+		
+		element(by.name("as_q")).sendKeys("E2E");
+		
+		element.all(by.repeater("item in value")).map(function(text,index) {
+			
+				console.log(" Tag Names : "+text.getTagName("a"));
+				
+				if(text.getTagName("a").getText() == "E2E Testing ")
+					{
+					 
+					console.log(" Found the element at index "+index);
+					
+					text.click();
+					 
+					}			
+			
+		});
+		
+		
+	});
+});
