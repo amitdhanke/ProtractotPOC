fc3a1929737248c8b452fcd2ceedf1a24fc08439
amitdhanke <amit.dhanke8@gmail.com>
2019-10-14 11:50:32 +0530

    JetBlue

--- src/scripts/ThirdProtractorScript.js
+++ src/scripts/ThirdProtractorScript.js
@@ -1,6 +1,6 @@
 describe("Drop Down Testsuite",function(){
-	
-	it("SPEC01",function(){
+		
+	xit("SPEC01",function(done){
 		
 		browser.get("https://angularjs.org/");
 		
@@ -23,26 +23,48 @@ describe("Drop Down Testsuite",function(){
 		});
 		
 		
-		
 		expect(element(by.xpath("//option[@selected='selected']")).getText()).toEqual("v1.6.10");
 		
 		element(by.name("as_q")).sendKeys("E2E");
 		
-		element.all(by.repeater("item in value")).map(function(text,index) {
+		element.all(by.xpath("//*[@ng-repeat='item in value']/a")).map(function(text,index) {
 			
-				console.log(" Tag Names : "+text.getTagName("a"));
+				console.log(" Tag Names : "+text[index].getText());
 				
-				if(text.getTagName("a").getText() == "E2E Testing ")
+				if(text[index].getText() == "E2E Testing ")
 					{
 					 
 					console.log(" Found the element at index "+index);
 					
-					text.click();
+					text[index].click();
 					 
 					}			
-			
+			done();
 		});
 		
 		
 	});
+	
+	it("SPEC02",function(done){
+		
+	//	browser.waitForAngularEnabled(false); // disabled the wait for angular base application
+		
+		browser.get("https://www.jetblue.com/");
+
+		//browser.navigate().refresh();
+		
+		browser.manage().window().maximize();
+		
+		element(by.id("jb-radio-1")).click();
+		
+		expect(element(by.id("jb-radio-1")).isSelected()).toBeTruthy();
+		
+		element(by.id("city-selector_wu7hfmuqn")).sendKeys("New York");
+		
+		element(by.xpath("//span[contains(text(),'Albany, NY (ALB)')]")).click();
+		
+		element(by.name("arrowRoundtrip")).click();
+		done(); // call done to avoid asynch timeout
+	}); 	
+	
 });
